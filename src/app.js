71063249e94da30aa68bbe3cbbbbfff38a4e5782
SolorzanoLuis <wolfsolorzano2000@gmail.com>
2022-05-29 21:02:26 -0500

    Actualizacion heroku

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 const express = require('express');
 require('./db/mongoose')
+var cors = require('cors')
 
 const userRouter = require('./routers/user')
 const productRouter =  require('./routers/product')
 const clientRouter = require('./routers/client')
 
 const app = express()
+const options = cors.CorsOptions = {
+    origin: '*'
+};
 
 app.use( express.json() )
+app.use(cors(options));
 app.use(userRouter)
 app.use(productRouter)
 app.use(clientRouter)
+
 
 
 module.exports = app
