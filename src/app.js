fc1554cfcad21c0f747cbcaea47603c1645d5c36
SolorzanoLuis <wolfsolorzano2000@gmail.com>
2022-07-22 01:23:35 -0500

    update

--- src/app.js
+++ src/app.js
@@ -1,25 +1,34 @@
 const express = require('express');
 require('./db/mongoose')
 const cors = require('cors');
+require('./db/populateData');
+
 
 const userRouter = require('./routers/user')
 const productRouter =  require('./routers/product')
 const clientRouter = require('./routers/client')
 const employeeRouter = require('./routers/employee')
 const hierarchyRouter = require('./routers/hierarchy')
+const usersRouter = require('./routers/users');
+const catalogsRouter = require('./routers/catalogs');
+const branchRouter = require('./routers/branch');
+const neighborhoodRouter = require('./routers/neighborhood');
 
 const app = express()
 const options = cors.CorsOptions = {
     origin: '*'
 };
 
-app.use( express.json({limit: '50mb'}) )
-app.use(cors(options));
+app.use(express.json({limit: '1mb'}))
 app.use(userRouter)
 app.use(productRouter)
 app.use(clientRouter)
 app.use(employeeRouter)
 app.use(hierarchyRouter)
+app.use(usersRouter)
+app.use(catalogsRouter)
+app.use(branchRouter)
+app.use(neighborhoodRouter)
 
 
 module.exports = app
